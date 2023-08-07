39f9f35d4fd684a831236aa107520d1d20ae1603
Anukul Poudel <54018611+AnukulPoudel@users.noreply.github.com>
2023-08-07 22:59:28 +0545

    Create installArch.sh

---
+++ installArch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asis/Kathmandu /etc/localtime
+hwclock --systohc
+sed -i '178s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# echo "KEYMAP=preferred_keymap" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra vde2 edk2-ovmf dnsmasq openbsd-netcat iptables-nft firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m anukul
+echo anukul:password | chpasswd
+usermod -aG libvirt anukul
+
+echo "anukul ALL=(ALL) ALL" >> /etc/sudoers.d/anukul
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
